46598658b93e0cc035460f3f01c1873c95472bda
SCHIMI485 <128907023+SCHIMI485@users.noreply.github.com>
2023-04-07 20:58:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
                 <table class="table table-striped">
                     <thead>
                         <tr>
+                            <th scope="col">Id</th>
                             <th scope="col">Nome</th>
                             <th scope="col">Endereço</th>
                             <th scope="col">Telefone</th>
